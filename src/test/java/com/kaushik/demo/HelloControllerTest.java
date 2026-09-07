fac9988e9ff40d8e52024ab2baceb0d735af0c85
kaushikmessi10 <kaushikmessi10@gmail.com>
2026-09-07 21:12:10 +0530

    initial spring boot app with Dockerfile

---
+++ src/test/java/com/kaushik/demo/HelloControllerTest.java
@@ -0,0 +1,23 @@
+package com.kaushik.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.kaushik.demo.HelloController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void rootReturns200() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk());
+    }
+}
