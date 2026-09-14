06011fdcd09b9c4c4ee81d2cb7119b93f3da7fbc
kaushikmessi10 <kaushikmessi10@gmail.com>
2026-09-14 14:36:08 +0530

    version 3

--- src/main/java/com/kaushik/demo/HelloController.java
+++ src/main/java/com/kaushik/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Jenkins pipeline! Version 2";
+        return "Hello from Jenkins pipeline! Version 3";
     }
 }
